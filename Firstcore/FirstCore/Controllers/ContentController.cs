f6b8ad099a6b39ba9cc9a63543f5f6fd390be445
logerlink <logerlink@outlook.com>
2019-05-25 16:05:38 +0800

    往往爱一个人有千百种可能，其滋味，不见得好过长夜孤枕

---
+++ Firstcore/FirstCore/Controllers/ContentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace FirstCore.Controllers
+{
+    public class ContentController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    var contents = new List<Content>();
+        //    for (int i = 1; i < 11; i++)
+        //    {
+        //        contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+        //    }
+        //    return View(new ContentViewModel() { Contents = contents });
+        //}
+
+        private readonly Content content;
+        public ContentController(IOptions<Content> option)
+        {
+            content = option.Value;
+        }
+        public IActionResult Index()
+        {
+            return View(new ContentViewModel() { Contents = new List<Content>() { content } });
+        }
+    }
+}
---
+++ Firstcore/FirstCore/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace FirstCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hosttingContext, config) =>
+            {
+                var env = hosttingContext.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile("Content.json", optional: false, reloadOnChange: false)
+                        .AddEnvironmentVariables();
+
+            })
+            .UseStartup<Startup>();
+
+    }
+}
